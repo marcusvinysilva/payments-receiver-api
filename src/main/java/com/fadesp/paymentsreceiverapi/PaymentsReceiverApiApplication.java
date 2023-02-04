d54461561ff357b390975e23215f4b84013e27ee
Marcus Silva <mvsilva1997@gmail.com>
2023-02-04 13:22:00 -0300

    feat: init project

---
+++ src/main/java/com/fadesp/paymentsreceiverapi/PaymentsReceiverApiApplication.java
@@ -0,0 +1,13 @@
+package com.fadesp.paymentsreceiverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsReceiverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsReceiverApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fadesp/paymentsreceiverapi/PaymentsReceiverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadesp.paymentsreceiverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsReceiverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
