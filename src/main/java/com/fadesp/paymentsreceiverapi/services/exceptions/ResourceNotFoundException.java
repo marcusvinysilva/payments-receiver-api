7a805b9d328b58b41d1c69907ccc82074fb540a8
Marcus Silva <mvsilva1997@gmail.com>
2023-02-05 19:26:59 -0300

    feat: add exception handling

---
+++ src/main/java/com/fadesp/paymentsreceiverapi/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.fadesp.paymentsreceiverapi.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+}
