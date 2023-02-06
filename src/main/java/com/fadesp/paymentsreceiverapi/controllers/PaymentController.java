b482cca1f597584c199480354a2ccd12c2e19477
Marcus Silva <mvsilva1997@gmail.com>
2023-02-05 22:18:47 -0300

    feat: add find all payments, create payment

--- src/main/java/com/fadesp/paymentsreceiverapi/controllers/PaymentController.java
+++ src/main/java/com/fadesp/paymentsreceiverapi/controllers/PaymentController.java
@@ -1,26 +1,36 @@
 package com.fadesp.paymentsreceiverapi.controllers;
 
+import java.net.URI;
 import java.util.List;
 
+import com.fadesp.paymentsreceiverapi.dto.PaymentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.fadesp.paymentsreceiverapi.dto.PaymentDTO;
+import com.fadesp.paymentsreceiverapi.dto.PaymentRequest;
 import com.fadesp.paymentsreceiverapi.services.PaymentService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import javax.xml.ws.Response;
 
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentController {
-	
-	@Autowired
-	private PaymentService service;
-	
-	@GetMapping
-	public ResponseEntity<List<PaymentDTO>> findAll() {
-		List<PaymentDTO> list = service.findAll();
-		return ResponseEntity.ok().body(list);
-	}
+
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping
+    public ResponseEntity<List<PaymentResponse>> findAll() {
+        List<PaymentResponse> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<PaymentResponse> insert(@RequestBody @Valid PaymentRequest payment) {
+        PaymentResponse newPayment = service.insert(payment);
+        return ResponseEntity.ok().body(newPayment);
+    }
 }
--- src/main/java/com/fadesp/paymentsreceiverapi/services/PaymentService.java
+++ src/main/java/com/fadesp/paymentsreceiverapi/services/PaymentService.java
@@ -3,24 +3,34 @@ package com.fadesp.paymentsreceiverapi.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.fadesp.paymentsreceiverapi.dto.PaymentResponse;
+import com.fadesp.paymentsreceiverapi.entities.mapper.PaymentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.fadesp.paymentsreceiverapi.dto.PaymentDTO;
+import com.fadesp.paymentsreceiverapi.dto.PaymentRequest;
 import com.fadesp.paymentsreceiverapi.entities.Payment;
 import com.fadesp.paymentsreceiverapi.repositories.PaymentRepository;
 
 @Service
 public class PaymentService {
 
-	@Autowired
-	private PaymentRepository repository;
+    @Autowired
+    private PaymentRepository repository;
 
-	@Transactional(readOnly = true)
-	public List<PaymentDTO> findAll() {
-		List<Payment> list = repository.findAll();
-		return list.stream().map(x -> new PaymentDTO(x)).collect(Collectors.toList());
-	}
+    @Autowired
+    private PaymentMapper paymentMapper;
 
+    @Transactional(readOnly = true)
+    public List<PaymentResponse> findAll() {
+        List<Payment> list = repository.findAll();
+        return list.stream().map(paymentMapper::responseToDto).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public PaymentResponse insert(PaymentRequest payment) {
+        Payment newPayment = paymentMapper.requestToEntity(payment);
+        return paymentMapper.responseToDto(repository.save(newPayment));
+    }
 }
