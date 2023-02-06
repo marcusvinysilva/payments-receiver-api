7a120849123c66f72fda30abed8904e888e6bdba
Marcus Silva <mvsilva1997@gmail.com>
2023-02-05 22:17:42 -0300

    refactor: remove string values

--- src/main/java/com/fadesp/paymentsreceiverapi/enums/PaymentMethodEnum.java
+++ src/main/java/com/fadesp/paymentsreceiverapi/enums/PaymentMethodEnum.java
@@ -2,19 +2,9 @@ package com.fadesp.paymentsreceiverapi.enums;
 
 public enum PaymentMethodEnum {
 
-	TICKET("boleto"),
-	PIX("pix"),
-	CREDIT_CARD("cartao_credito"),
-	DEBIT_CARD("cartao_debito");
-	
-	private String value;
-	
-	private PaymentMethodEnum(String value) {
-		this.value = value;
-	}
-
-	public String getValue() {
-		return value;
-	}
+	BOLETO,
+	PIX,
+	CARTAO_CREDITO,
+	CARTAO_DEBITO
 	
 }
--- src/main/java/com/fadesp/paymentsreceiverapi/enums/PaymentStatus.java
+++ src/main/java/com/fadesp/paymentsreceiverapi/enums/PaymentStatus.java
@@ -1,19 +1,9 @@
 package com.fadesp.paymentsreceiverapi.enums;
 
 public enum PaymentStatus {
-	
-	SUCESS("sucesso"),
-	FAILURE("falha"),
-	PENDING("pendente");
-	
-	private String value;
 
-	private PaymentStatus(String value) {
-		this.value = value;
-	}
-
-	public String getValue() {
-		return value;
-	}
+    SUCESSO,
+    FALHA,
+    PENDENTE
 
 }
